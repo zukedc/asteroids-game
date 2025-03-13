df93f5f4adfc7b7bf49429cc0d98f3927c23149e
zukedc <zuke_dc@yahoo.com>
2025-03-13 14:32:10 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+
+if __name__ == "__main__":
+    main()
